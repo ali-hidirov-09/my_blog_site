2928263d933628b0ecb8c587a33da2216fcaa450
Khidirov Ali <alihidirov285@gmail.com>
2025-10-04 16:02:31 +0600

    Code improved

--- blogapp/views.py
+++ blogapp/views.py
@@ -6,10 +6,20 @@ from django.views.generic import ListView
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by = 5   # bir page da 5 ta maqola bor
+    paginate_by = 3  # bir page da 3 ta maqola bor
     template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, slug):
-    post = get_object_or_404(Post, slug=slug, status="published", publish__year=year, publish__month=month, publish__day=day)
+    post = get_object_or_404(Post, slug=slug,
+                             status="published",
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
